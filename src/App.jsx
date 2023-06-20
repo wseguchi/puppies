37a700ffadca26e6c8a6f6dc9b6b9a4fa4752aeb
Wildson Seguchi <wseguchi@gmail.com>
2023-06-20 11:21:57 -0500

    Created basic components. Header done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import Api from '../api-tests/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      {/* Wrapping routes */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
