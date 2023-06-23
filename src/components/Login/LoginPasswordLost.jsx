9d2d0e9354b0eae5e8a570857239e7d25b1aedf3
Wildson Seguchi <wseguchi@gmail.com>
2023-06-22 23:44:45 -0500

    Password Lost and Recovery

--- src/components/Login/LoginPasswordLost.jsx
+++ src/components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../Api';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace('lost', 'reset'),
+      });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className='title'>Lost your password?</h1>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input
+            label='E-mail or Username'
+            type='text'
+            name='login'
+            {...login}
+          />
+          {loading ? (
+            <Button disabled>Loading...</Button>
+          ) : (
+            <Button>Send e-mail</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
